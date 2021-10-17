936ef5b9624643fb7e58a99523e95c23a1f4f193
Ashley McCallum <w0765596@myscc.ca>
2021-10-17 00:29:42 -0400

    #2 Add buttons to MainTablePane

--- src/main/java/Panes/MainTablePane.java
+++ src/main/java/Panes/MainTablePane.java
@@ -1,10 +1,28 @@
 package Panes;
 
+import Scenes.AddItemScene;
+import com.example.thelorestore.Launcher;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
 public class MainTablePane extends StackPane {
 
     public MainTablePane() {
+
+        HBox buttons = new HBox();
+        Button addItemButton = new Button("Add Item");
+        addItemButton.setOnAction(e -> {
+            Launcher.mainStage.setScene(new AddItemScene());
+        });
+
+        Button viewItemButton = new Button("View Item");
+        viewItemButton.setOnAction(e -> {
+            //TODO - connect button to update/view scene
+        });
+        buttons.getChildren().addAll(addItemButton, viewItemButton);
+
+        this.getChildren().addAll(buttons);
 
     }
 }
