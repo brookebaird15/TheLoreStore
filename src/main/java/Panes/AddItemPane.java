4d915f3a94f6ab8c1f5bf80f4b479f51827f6e60
Ashley McCallum <w0765596@myscc.ca>
2021-10-16 23:54:50 -0400

    Add prompt text and buttons to AddItemPane

--- src/main/java/Panes/AddItemPane.java
+++ src/main/java/Panes/AddItemPane.java
@@ -1,6 +1,9 @@
 package Panes;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
@@ -10,13 +13,28 @@ public class AddItemPane extends StackPane {
         VBox inputFields = new VBox();
 
         TextField title = new TextField();
+        title.setPromptText("Title");
         TextField author = new TextField();
+        author.setPromptText("Author");
         TextField genre = new TextField();
+        genre.setPromptText("Genre");
         TextField publisher = new TextField();
+        publisher.setPromptText("Publisher");
         TextField publishYear = new TextField();
+        publishYear.setPromptText("Year Published");
         TextField quantity = new TextField();
+        quantity.setPromptText("Stock Qty");
 
-        inputFields.getChildren().addAll(title, author, genre, publisher, publishYear, quantity);
+        HBox buttons = new HBox();
+        Button addButton = new Button("Add Item");
+        Button cancelButton = new Button("Cancel");
+        buttons.getChildren().addAll(addButton, cancelButton);
+
+        inputFields.getChildren().addAll(title, author, genre, publisher, publishYear, quantity, buttons);
+        inputFields.setAlignment(Pos.CENTER);
+
+
+
 
         this.getChildren().addAll(inputFields);
 
