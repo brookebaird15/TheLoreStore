7a1b7aa83833a572288bb96bd0f6a5931aeeecfc
Brooke Baird <w0766373@myscc.ca>
2021-10-18 00:19:53 -0400

    Add Vboxes for input fields

--- src/main/java/Panes/UpdateItemPane.java
+++ src/main/java/Panes/UpdateItemPane.java
@@ -1,6 +1,20 @@
 package Panes;
 
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class UpdateItemPane extends StackPane {
+    public UpdateItemPane() {
+        //A vbox to hold all the input boxes
+        VBox inputFieldsBox = new VBox();
+
+        //Vbox to hold title of book
+        VBox title = new VBox();
+
+        //Vbox to hold author
+        //Vbox to hold genre
+        //Vbox to hold publisher
+        //Vbox to hold year published
+        //Vbox to hold stock amount
+    }
 }
