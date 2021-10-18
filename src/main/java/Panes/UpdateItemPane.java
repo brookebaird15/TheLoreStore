5cada8fd8b991b974abebdf4a7214f00c53d98bc
Brooke Baird <w0766373@myscc.ca>
2021-10-18 00:27:28 -0400

    Added texts for each input

--- src/main/java/Panes/UpdateItemPane.java
+++ src/main/java/Panes/UpdateItemPane.java
@@ -1,16 +1,16 @@
 package Panes;
 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class UpdateItemPane extends StackPane {
     public UpdateItemPane() {
         //A vbox to hold all the input boxes
         VBox inputFieldsBox = new VBox();
-
         //Vbox to hold title of book
         VBox title = new VBox();
-
         //Vbox to hold author
         VBox author = new VBox();
         //Vbox to hold genre
@@ -21,5 +21,15 @@ public class UpdateItemPane extends StackPane {
         VBox yearPublished = new VBox();
         //Vbox to hold stock amount
         VBox stockQuantity = new VBox();
+
+        //Texts for each input
+        Text titleTxt = new Text("Title");
+        Text authorTxt = new Text("Author");
+        Text genreTxt = new Text("Genre");
+        Text publisherTxt = new Text("Publisher");
+        Text yearTxt = new Text("Year Published");
+        Text stockTxt = new Text("Stock Qty");
+
+      
     }
 }
