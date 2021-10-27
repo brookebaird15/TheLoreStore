a82070b8ec94e7af38a6e6faeb9cf183ef807bd5
Brooke Baird <w0766373@myscc.ca>
2021-10-27 15:53:06 -0400

    Create table column names

---
+++ src/main/java/com/example/thelorestore/DBTableValues.java
@@ -0,0 +1,35 @@
+package com.example.thelorestore;
+
+public class DBTableValues {
+    //Publisher Table
+    public static final String PUBLISHER_TABLE = "publisher";
+    public static final String PUBLISHER_ID_COLUMN = "id";
+    public static final String PUBLISHER_COMPANY_COLUMN = "company_name";
+
+    //Genre Table
+    public static final String GENRE_TABLE = "genre";
+    public static final String GENRE_ID_COLUMN = "id";
+    public static final String GENRE_NAME_COLUMN = "genre";
+
+    //Author Table
+    public static final String AUTHOR_TABLE = "author";
+    public static final String AUTHOR_ID_COLUMN = "id";
+    public static final String AUTHOR_FIRST_COLUMN = "first_name";
+    public static final String AUTHOR_MIDDLE_COLUMN = "middle_name";
+    public static final String AUTHOR_LAST_COLUMN = "last_name";
+    public static final String AUTHOR_PREFIX_COLUMN = "prefix";
+    public static final String AUTHOR_SUFFIX_COLUMN = "suffix";
+
+    //Item Table
+    public static final String BOOK_TABLE = "book";
+    public static final String BOOK_ID_COLUMN = "id";
+    public static final String BOOK_TITLE_COLUMN = "title";
+    public static final String BOOK_AUTHOR_COLUMN = "author";
+    public static final String BOOK_GENRE_COLUMN = "genre";
+    public static final String BOOK_PUBLISHER_COLUMN = "publisher";
+    public static final String BOOK_YEAR_COLUMN = "year_published";
+    public static final String BOOK_QUANTITY_COLUMN = "quantity";
+
+    
+
+}
