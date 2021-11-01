21c9806e8e5fbd34bcb395b4ab505b5e8fb6b28d
Brooke Baird <w0766373@myscc.ca>
2021-10-31 22:55:39 -0400

    #26 Create Main Table - Rearranged other tables in order of how they appear in the main table

--- src/main/java/com/example/thelorestore/DBTableValues.java
+++ src/main/java/com/example/thelorestore/DBTableValues.java
@@ -20,7 +20,7 @@ public class DBTableValues {
     public static final String AUTHOR_PREFIX_COLUMN = "prefix";
     public static final String AUTHOR_SUFFIX_COLUMN = "suffix";
 
-    //Item Table
+    //Book Table
     public static final String BOOK_TABLE = "book";
     public static final String BOOK_ID_COLUMN = "id";
     public static final String BOOK_TITLE_COLUMN = "title";
@@ -35,8 +35,24 @@ public class DBTableValues {
      * PUBLISHER TABLE - PRIMARY KEY, COMPANY
      * GENRE TABLE - PRIMARY KEY, NAME
      * AUTHOR TABLE - PRIMARY KEY, FIRST_NAME, MIDDLE_NAME, LAST_NAME, PREFIX, SUFFIX
-     * ITEM TABLE - PRIMARY KEY, TITLE, AUTHOR, GENRE, PUBLISHER, YEAR_PUBLISHED, QUANTITY
+     * BOOK TABLE - PRIMARY KEY, TITLE, AUTHOR, GENRE, PUBLISHER, YEAR_PUBLISHED, QUANTITY
      */
+
+    public static final String CREATE_AUTHOR_TABLE =
+            "CREATE TABLE " + AUTHOR_TABLE + " ("
+                    + AUTHOR_ID_COLUMN + " int NOT NULL AUTO_INCREMENT, "
+                    + AUTHOR_FIRST_COLUMN + " VARCHAR(50) NOT NULL, "
+                    + AUTHOR_MIDDLE_COLUMN + " VARCHAR(50), "
+                    + AUTHOR_LAST_COLUMN + " VARCHAR(50) NOT NULL, "
+                    + AUTHOR_PREFIX_COLUMN + " VARCHAR(10), "
+                    + AUTHOR_SUFFIX_COLUMN + " VARCHAR(10), "
+                    + "PRIMARY KEY(" + AUTHOR_ID_COLUMN + ")" + ");";
+
+    public static final String CREATE_GENRE_TABLE =
+            "CREATE TABLE " + GENRE_TABLE + " ("
+                    + GENRE_ID_COLUMN + " int NOT NULL AUTO_INCREMENT, "
+                    + GENRE_NAME_COLUMN + " VARCHAR(30) NOT NULL, "
+                    + "PRIMARY KEY(" + GENRE_ID_COLUMN + ")" + ");";
 
     public static final String CREATE_PUBLISHER_TABLE =
             "CREATE TABLE " + PUBLISHER_TABLE + " ("
@@ -44,21 +60,19 @@ public class DBTableValues {
             + PUBLISHER_COMPANY_COLUMN + " VARCHAR(50) NOT NULL, "
             + "PRIMARY KEY(" + PUBLISHER_ID_COLUMN + ")" + ");";
 
-    public static final String CREATE_GENRE_TABLE =
-            "CREATE TABLE " + GENRE_TABLE + " ("
-            + GENRE_ID_COLUMN + " int NOT NULL AUTO_INCREMENT, "
-            + GENRE_NAME_COLUMN + " VARCHAR(30) NOT NULL, "
-            + "PRIMARY KEY(" + GENRE_ID_COLUMN + ")" + ");";
-
-    public static final String CREATE_AUTHOR_TABLE =
-            "CREATE TABLE " + AUTHOR_TABLE + " ("
-            + AUTHOR_ID_COLUMN + " int NOT NULL AUTO_INCREMENT, "
-            + AUTHOR_FIRST_COLUMN + " VARCHAR(50) NOT NULL, "
-            + AUTHOR_MIDDLE_COLUMN + " VARCHAR(50), "
-            + AUTHOR_LAST_COLUMN + " VARCHAR(50) NOT NULL, "
-            + AUTHOR_PREFIX_COLUMN + " VARCHAR(10), "
-            + AUTHOR_SUFFIX_COLUMN + " VARCHAR(10), "
-            + "PRIMARY KEY(" + AUTHOR_ID_COLUMN + ")" + ");";
+    public static final String CREATE_ITEM_TABLE =
+            "CREATE TABLE " + BOOK_TABLE + "("
+            + BOOK_ID_COLUMN + " int NOT NULL AUTO INCREMENT PRIMARY KEY,"
+            + BOOK_TITLE_COLUMN + " VARCHAR(255) NOT NULL, "
+            + BOOK_AUTHOR_COLUMN + " VARCHAR(255) NOT NULL, "
+            + BOOK_GENRE_COLUMN + " VARCHAR(50) NOT NULL, "
+            + BOOK_PUBLISHER_COLUMN + " VARCHAR(255) NOT NULL, "
+            + BOOK_YEAR_COLUMN + " int(4) NOT NULL, "
+            + BOOK_QUANTITY_COLUMN + " int, "
+            + "FOREIGN KEY (" + BOOK_AUTHOR_COLUMN + ")" + " REFERENCES " + AUTHOR_TABLE + "(" + AUTHOR_ID_COLUMN + "),"
+            + "FOREIGN KEY (" + BOOK_GENRE_COLUMN + ")" + " REFERENCES " + GENRE_TABLE + "(" + GENRE_ID_COLUMN + "),"
+            + "FOREIGN KEY (" + BOOK_PUBLISHER_COLUMN + ")" + " REFERENCES " + PUBLISHER_TABLE + "(" + PUBLISHER_ID_COLUMN + "),"
+            + ");";
 
 
 }
