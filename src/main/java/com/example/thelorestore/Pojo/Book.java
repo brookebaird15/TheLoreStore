edbd0e234331f83b77274ca10974ebae58f6d24a
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 15:24:18 -0400

    #35 Add content to Book pojo * Add variables

--- src/main/java/com/example/thelorestore/Pojo/Book.java
+++ src/main/java/com/example/thelorestore/Pojo/Book.java
@@ -1,4 +1,11 @@
 package com.example.thelorestore.Pojo;
 
 public class Book {
+    private int id;
+    private String title;
+    private int author;
+    private int genre;
+    private int publisher;
+    private int year;
+    private int quantity;
 }
