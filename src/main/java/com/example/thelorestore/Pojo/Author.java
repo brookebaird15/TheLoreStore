e4de661eed6ab3d68571345eaf0b0289c37bc076
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 15:17:39 -0400

    #33 Add content to Author pojo * Add variables, constructor, getters, setters, toString

--- src/main/java/com/example/thelorestore/Pojo/Author.java
+++ src/main/java/com/example/thelorestore/Pojo/Author.java
@@ -1,4 +1,72 @@
 package com.example.thelorestore.Pojo;
 
 public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String prefix;
+    private String suffix;
+
+    public Author(int id, String firstName, String lastName, String middleName, String prefix, String suffix) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String toString() {
+        return prefix + " " + firstName + " " + middleName + " " + lastName + " " + suffix;
+    }
 }
