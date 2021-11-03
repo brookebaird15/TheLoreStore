7cf537e0c37a6672b4c0ccf413df784af24517f7
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 15:44:36 -0400

    #31 Add content to Book DAO
    * Added createBook, updateBook, deleteBook methods for altering table
    * Added getAllBooks, getBook to view items in table

--- src/main/java/com/example/thelorestore/DAOs/BookDAO.java
+++ src/main/java/com/example/thelorestore/DAOs/BookDAO.java
@@ -1,4 +1,36 @@
 package com.example.thelorestore.DAOs;
 
+import com.example.thelorestore.Pojo.Book;
+
+import java.util.ArrayList;
+
 public interface BookDAO {
+    /**
+     * Method for creating a new entry to the Book Table
+     * @param book
+     */
+    public void createBook(Book book);
+
+    /**
+     * Method for accessing all books in Book Table
+     */
+    public ArrayList<Book> getAllBooks();
+
+    /**
+     * Method for accessing one book in Book Table
+     * @param id
+     */
+    public Book getBook(int id);
+
+    /**
+     * Method for updating book information in Book Table
+     * @param book
+     */
+    public void updateBook(Book book);
+
+    /**
+     * Method for removing a book from the Book Table
+     * @param book
+     */
+    public void deleteBook(Book book);
 }
