84cafaf3c54ae4ff92c78f000942f01aab2e5ee9
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 15:40:07 -0400

    #30 Add content to AuthorDAO * Create methods for accessing all authors and specific authors

--- src/main/java/com/example/thelorestore/DAOs/AuthorDAO.java
+++ src/main/java/com/example/thelorestore/DAOs/AuthorDAO.java
@@ -1,4 +1,13 @@
 package com.example.thelorestore.DAOs;
 
+import com.example.thelorestore.Pojo.Author;
+
+import java.util.ArrayList;
+
 public interface AuthorDAO {
+    /**
+     * Methods for accessing Author Table
+     */
+    public ArrayList<Author> getAllAuthors();
+    public Author getAuthor(int id);
 }
