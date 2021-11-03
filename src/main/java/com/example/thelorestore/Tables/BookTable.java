dd142c7e8453dd2229b69f581be2a4f1f4301e9a
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 16:19:05 -0400

    #38 Add createBook() to BookTable

--- src/main/java/com/example/thelorestore/Tables/BookTable.java
+++ src/main/java/com/example/thelorestore/Tables/BookTable.java
@@ -1,4 +1,64 @@
 package com.example.thelorestore.Tables;
 
-public class BookTable {
+import com.example.thelorestore.DAOs.BookDAO;
+import com.example.thelorestore.Database.DBTableValues;
+import com.example.thelorestore.Database.Database;
+import com.example.thelorestore.Pojo.Book;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BookTable implements BookDAO {
+
+    Database db = Database.getInstance();
+    ArrayList<Book> books;
+
+    /**
+     * createBook() adds a book to the Book Table
+     * @param book is the book being added
+     */
+    @Override
+    public void createBook(Book book) {
+        String query = "INSERT INTO " + DBTableValues.BOOK_TABLE + "(" +
+                DBTableValues.BOOK_TITLE_COLUMN + ", " +
+                DBTableValues.BOOK_AUTHOR_COLUMN + ", " +
+                DBTableValues.BOOK_GENRE_COLUMN + ", " +
+                DBTableValues.BOOK_PUBLISHER_COLUMN + ", " +
+                DBTableValues.BOOK_YEAR_COLUMN + ", " +
+                DBTableValues.BOOK_QUANTITY_COLUMN + ", " +
+                DBTableValues.BOOK_PRICE_COLUMN + ") VALUES ('" +
+                book.getTitle() + "','" +
+                book.getAuthor() + "','" +
+                book.getGenre() + "','" +
+                book.getPublisher() + "','" +
+                book.getYear() + "','" +
+                book.getQuantity() + "','" +
+                book.getPrice() + "')";
+        try {
+            db.getConnection().createStatement().execute(query);
+            System.out.println("Record inserted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ArrayList<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public Book getBook(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateBook(Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(Book book) {
+
+    }
 }
