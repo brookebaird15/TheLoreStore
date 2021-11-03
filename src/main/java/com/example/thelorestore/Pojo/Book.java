dde214bccc53edb5151e3af19ce87b880b9d7280
Ashley McCallum <w0765596@myscc.ca>
2021-11-03 15:30:39 -0400

    #35 Add content to Book pojo * Add constructors, getters, setters, toString

--- src/main/java/com/example/thelorestore/Pojo/Book.java
+++ src/main/java/com/example/thelorestore/Pojo/Book.java
@@ -8,4 +8,95 @@ public class Book {
     private int publisher;
     private int year;
     private int quantity;
+    private double price;
+
+    public Book(int id, String title, int author, int genre, int publisher, int year, int quantity, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.year = year;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Book(String title, int author, int genre, int publisher, int year, int quantity, double price) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.year = year;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
+    }
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
+    public int getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(int publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
