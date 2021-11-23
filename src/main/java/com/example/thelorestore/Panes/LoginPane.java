020197cfaff3cae184abbef8713165575b594e4f
Brooke Baird <w0766373@myscc.ca>
2021-11-23 00:09:44 -0500

    #20 Add image to background

--- src/main/java/com/example/thelorestore/Panes/LoginPane.java
+++ src/main/java/com/example/thelorestore/Panes/LoginPane.java
@@ -2,55 +2,63 @@ package com.example.thelorestore.Panes;
 
 import com.example.thelorestore.Scenes.MainTableScene;
 import com.example.thelorestore.Launcher;
+import javafx.animation.SequentialTransition;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 //Extend StackPane
 public class LoginPane extends StackPane {
     public LoginPane(){
-        /*** TODO
-         * Add imageview that expands top half of login screen, animated on a loop going back and forth slowly
-         */
+
         Text user = new Text("Username");
         Text password = new Text("Password");
 
-        //New login button
+        ImageView booksImage = new ImageView(new Image("file:Images/booksLogin.jpg"));
+
+//        New login button
         Button loginBtn = new Button("Log in");
         //Event Handling of login button to the main table
         loginBtn.setOnAction(event -> {
-            Launcher.mainStage.setScene(new MainTableScene());
-            /*** TODO
-             * Check login credentials for logging into database
-             */
-        });
+                    Launcher.mainStage.setScene(new MainTableScene());
+                    /*** TODO
+                     * Check login credentials for logging into database
+                     */
+                });
+            TextField userTextField = new TextField();
+            TextField pwTextField = new TextField();
 
-        TextField userTextField = new TextField();
-        TextField pwTextField = new TextField();
+            //The vboxes used to set content in middle, holds username & password
+            VBox usernameBox = new VBox();
+            VBox passwordBox = new VBox();
+            usernameBox.getChildren().addAll(user, userTextField);
+            passwordBox.getChildren().addAll(password, pwTextField);
 
-        //The vboxes used to set content in middle, holds username & password
-        VBox usernameBox = new VBox();
-        VBox passwordBox = new VBox();
-        usernameBox.getChildren().addAll(user, userTextField);
-        passwordBox.getChildren().addAll(password, pwTextField);
-
-        //Setting the alignments for username & password vboxes
-        usernameBox.setAlignment(Pos.CENTER);
-        passwordBox.setAlignment(Pos.CENTER);
+            //Setting the alignments for username & password vboxes
+            usernameBox.setAlignment(Pos.CENTER);
+            passwordBox.setAlignment(Pos.CENTER);
 
 
-        //Holds all the username and password vboxes
-        VBox loginBox = new VBox();
-        loginBox.getChildren().addAll(usernameBox, passwordBox, loginBtn);
-        loginBox.setAlignment(Pos.CENTER);
-        loginBox.setSpacing(10);
-        loginBox.setMaxWidth(300);
-        this.getChildren().addAll(loginBox);
+            //Holds all the username and password vboxes
+            VBox loginBox = new VBox();
+            loginBox.getChildren().addAll(usernameBox, passwordBox, loginBtn);
+            loginBox.setAlignment(Pos.CENTER);
+            loginBox.setSpacing(10);
+            loginBox.setMaxWidth(200);
 
-    }
+            this.getChildren().addAll(booksImage, loginBox);
+
+        //Animations for background
+        SequentialTransition moveImage = new SequentialTransition();
+            moveImage.getChildren().addAll(
+            )
+        }
+
+
 
 
 }
