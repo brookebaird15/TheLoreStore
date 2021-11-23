599895cd7ed97a05301c72f0ae9d203bbe72eb8d
Ashley McCallum <w0765596@myscc.ca>
2021-11-22 22:55:12 -0500

    #51 Add genre tableview
    * Genres now listed in genre tab

--- src/main/java/com/example/thelorestore/Panes/Tabs/GenreTab.java
+++ src/main/java/com/example/thelorestore/Panes/Tabs/GenreTab.java
@@ -1,12 +1,32 @@
 package com.example.thelorestore.Panes.Tabs;
 
+import com.example.thelorestore.Pojo.Genre;
+import com.example.thelorestore.Tables.GenreTable;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 public class GenreTab extends Tab {
     private static GenreTab tab;
+    public TableView tableView;
 
     private GenreTab() {
         this.setText("Genre");
+        GenreTable genreTable = new GenreTable();
+        BorderPane root = new BorderPane();
+        tableView = new TableView();
+
+        TableColumn<Genre, String> genreColumn = new TableColumn<>("Genre");
+        genreColumn.setCellValueFactory(e-> new SimpleStringProperty(e.getValue().getName()));
+
+        tableView.getColumns().addAll(genreColumn);
+        tableView.getItems().addAll(genreTable.getAllGenres());
+        genreColumn.setPrefWidth(700);
+        root.setCenter(tableView);
+
+        this.setContent(root);
     }
 
     public static GenreTab getInstance() {
