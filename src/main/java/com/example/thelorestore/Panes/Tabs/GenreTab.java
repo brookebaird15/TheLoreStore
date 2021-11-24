6c7cf3f77054c51b5ccd01ef3936c2207d63509c
Brooke Baird <w0766373@myscc.ca>
2021-11-23 23:04:15 -0500

    #6, #15 Add 'add' function to genre tab - Genre table can be added to and it's content saved - Create refreshGenreTable function

--- src/main/java/com/example/thelorestore/Panes/Tabs/GenreTab.java
+++ src/main/java/com/example/thelorestore/Panes/Tabs/GenreTab.java
@@ -3,20 +3,49 @@ package com.example.thelorestore.Panes.Tabs;
 import com.example.thelorestore.Pojo.Genre;
 import com.example.thelorestore.Tables.GenreTable;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class GenreTab extends Tab {
     private static GenreTab tab;
     public TableView tableView;
+    public TextField genreField;
+    public Button addGenreBtn;
+    public Button saveAddBtn;
+    public Button updateGenreBtn;
+    public Button saveUpdateBtn;
 
     private GenreTab() {
         this.setText("Genre");
         GenreTable genreTable = new GenreTable();
         BorderPane root = new BorderPane();
         tableView = new TableView();
+
+        /** HBOX **/
+        HBox editButtons = new HBox();
+
+        /** BUTTONS **/
+        //Allows user to add a genre
+        addGenreBtn = new Button("Add Genre");
+        addGenreBtn.setOnAction(event -> {
+            genreField.setVisible(true);
+            saveAddBtn.setVisible(true);
+            updateGenreBtn.setDisable(true);
+        });
+
+        //Save Button for Adding
+        saveAddBtn = new Button();
+        saveAddBtn.setOnAction(event -> {
+            Genre genre = new Genre(genreField.getText());
+            genreTable.createGenre(genre);
+            saveAddBtn.setVisible(false);
+            genreField.setVisible(false);
+            refreshGenreTable();
+            updateGenreBtn.setDisable(true);
+
+
+        });
 
         TableColumn<Genre, String> genreColumn = new TableColumn<>("Genre");
         genreColumn.setCellValueFactory(e-> new SimpleStringProperty(e.getValue().getName()));
@@ -27,6 +56,9 @@ public class GenreTab extends Tab {
 
         root.setCenter(tableView);
 
+        //Set genreField to false visibility
+        genreField.setVisible(false);
+
         this.setContent(root);
     }
 
@@ -36,4 +68,14 @@ public class GenreTab extends Tab {
         }
         return tab;
     }
+
+    /**
+     * Method to refresh the table of it's values and place them inside a tableview
+     */
+    public void refreshGenreTable(){
+        GenreTable table = new GenreTable();
+        tableView.getItems().clear();
+        tableView.getItems().addAll(table.getAllGenres());
+    }
 }
+
