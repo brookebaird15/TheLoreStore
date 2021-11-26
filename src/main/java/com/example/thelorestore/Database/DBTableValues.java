4a288a6f6b754548f485d6ea230b8268326c3bf8
Ashley McCallum <w0765596@myscc.ca>
2021-11-25 19:15:05 -0500

    Add missing publisher column to table

--- src/main/java/com/example/thelorestore/Database/DBTableValues.java
+++ src/main/java/com/example/thelorestore/Database/DBTableValues.java
@@ -42,7 +42,7 @@ public class DBTableValues {
     public static final String BOOK_STATUS_COLUMN = "status";
     public static final String BOOK_COMMENT_COLUMN = "comment";
 
-    public static final String BOOK_VIEW = "book_info";
+    public static final String BOOK_VIEW = "book_info1";
     public static final String BOOK_VIEW_ID = "id";
     public static final String BOOK_VIEW_TITLE = "title";
     public static final String BOOK_VIEW_AUTHOR = "author_name";
@@ -129,11 +129,11 @@ public class DBTableValues {
                     + BOOK_VIEW_COMMENT + " FROM "
                     + BOOK_VIEW_GENRE + " AS g "
                     + "INNER JOIN " + BOOK_GENRE_TABLE + " AS bgr ON g." + GENRE_ID_COLUMN + "=bgr." + GENRE_FK_ID_COLUMN
-                    + "INNER JOIN " + BOOK_TABLE + " AS b ON bgr." + BOOK_ID_COLUMN_FOR_GENRE + "=b." + BOOK_ID_COLUMN
-                    + "INNER JOIN " + PUBLISHER_TABLE + " AS p ON p." + PUBLISHER_ID_COLUMN + "=b." + BOOK_PUBLISHER_COLUMN
-                    + "INNER JOIN " + STATUS_TABLE + " AS s ON s." + STATUS_ID_COLUMN + "=b." + BOOK_STATUS_COLUMN
-                    + "INNER JOIN " + BOOK_AUTHOR_TABLE + " AS bar ON b." + BOOK_ID_COLUMN + "=bar." + BOOK_ID_COLUMN_FOR_AUTHOR
-                    + "INNER JOIN " + AUTHOR_TABLE + " AS a ON bar." + AUTHOR_FK_ID_COLUMN + "=a." + AUTHOR_ID_COLUMN;
+                    + " INNER JOIN " + BOOK_TABLE + " AS b ON bgr." + BOOK_ID_COLUMN_FOR_GENRE + "=b." + BOOK_ID_COLUMN
+                    + " INNER JOIN " + PUBLISHER_TABLE + " AS p ON p." + PUBLISHER_ID_COLUMN + "=b." + BOOK_PUBLISHER_COLUMN
+                    + " INNER JOIN " + STATUS_TABLE + " AS s ON s." + STATUS_ID_COLUMN + "=b." + BOOK_STATUS_COLUMN
+                    + " INNER JOIN " + BOOK_AUTHOR_TABLE + " AS bar ON b." + BOOK_ID_COLUMN + "=bar." + BOOK_ID_COLUMN_FOR_AUTHOR
+                    + " INNER JOIN " + AUTHOR_TABLE + " AS a ON bar." + AUTHOR_FK_ID_COLUMN + "=a." + AUTHOR_ID_COLUMN;
     //CREATE VIEW book_info AS SELECT b.title, CONCAT(a.first_name, ' ', COALESCE(a.middle_name, ""), ' ', a.last_name) AS author_name,
     // g.genre, p.company_name, b.year_published, s.status, COALESCE(b.comment, "") AS comment
     //FROM genre AS g INNER JOIN book_genre_relation AS bgr
